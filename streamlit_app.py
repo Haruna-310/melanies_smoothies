51c398d4ee62028cc1c101b8ce5ec11b1f5b00cb
Haruna-310 <h.ishii@sbf.co.jp>
2024-10-30 17:18:34 +0900

    Update streamlit_app.py
    add 49-51

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
                                                                       
 #複数選択機能
 ingredient_list = st.multiselect(
@@ -45,6 +45,11 @@ if ingredient_list:
     #選択したものに次に選択したものをくっつける
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen+ ' '
+
+        #SEARCH_ONの値を取得
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
